e2f1e204a4aea6728ccaa07b040e91cc680ac09e
suyogangaj21 <sma6021532@gmail.com>
2025-10-10 16:55:49 +0530

    fix:remove unused files

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -18,7 +18,6 @@ export default function Header() {
       </div>
 
       <div className='flex items-center gap-2 px-4'>
-        <CtaGithub />
         <div className='hidden md:flex'>
           <SearchInput />
         </div>
